09564ad833abaec38b709e4be9136bd72b43e179
Ivaylo Mutafchiev <stu1901322006@uni-plovdiv.bg>
2020-12-07 17:05:20 +0200

    Update to angular 11

--- Technostore/src/app/details-category/details-category.component.spec.ts
+++ Technostore/src/app/details-category/details-category.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { DetailsCategoryComponent } from './details-category.component';
 
@@ -6,7 +6,7 @@ describe('DetailsCategoryComponent', () => {
   let component: DetailsCategoryComponent;
   let fixture: ComponentFixture<DetailsCategoryComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ DetailsCategoryComponent ]
     })
